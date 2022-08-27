0e61be5fbc09f2abf8fb8cec04b54b673527040c
Verania Calderon <102995385+veraniacalderon@users.noreply.github.com>
2022-08-26 17:15:18 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from cProfile import run
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+export FLASK_APP=app.py
+set FLASK_APP=app.py
+flask run
